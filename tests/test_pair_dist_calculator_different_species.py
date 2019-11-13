c1036e944a447fc8aec9ce603d4f692c802562d2
Oliver K. Ernst <oernst@ucsd.edu>
2019-11-12 16:48:32 -0800

    tests for remove all particles of some species

--- tests/test_pair_dist_calculator_different_species.py
+++ tests/test_pair_dist_calculator_different_species.py
@@ -404,6 +404,34 @@ class TestDifferentSpecies:
         assert pdc.n_species_A == 0
         assert pdc.n_species_B == 0
 
+    def test_remove_all_particles_of_species_A(self):
+
+        self.make_particles()
+
+        cutoff_dist = 0.3
+        pdc = PairDistCalculatorDifferentSpecies(self.posns_A, self.posns_B, self.dim, cutoff_dist=cutoff_dist)
+
+        pdc.remove_all_particles_of_species_A()
+
+        assert pdc.n_species_A == 0
+        assert pdc.n_species_B != 0
+        assert pdc.no_pairs == 0
+        assert pdc.no_pairs_within_cutoff == 0
+
+    def test_remove_all_particles_of_species_B(self):
+
+        self.make_particles()
+
+        cutoff_dist = 0.3
+        pdc = PairDistCalculatorDifferentSpecies(self.posns_A, self.posns_B, self.dim, cutoff_dist=cutoff_dist)
+
+        pdc.remove_all_particles_of_species_B()
+
+        assert pdc.n_species_A != 0
+        assert pdc.n_species_B == 0
+        assert pdc.no_pairs == 0
+        assert pdc.no_pairs_within_cutoff == 0
+
     def test_compute_dists_squared_between_particle_of_species_A_and_existing(self):
 
         self.make_particles()
